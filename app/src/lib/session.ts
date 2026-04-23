0419351a7c1ad9ec3d61516278ee56b5b59325e1
hrzhkm <harizhakim85@gmail.com>
2026-04-23 20:25:42 +0800

    fix: use getRequest from react-start/server instead of internal start-server-core

--- app/src/lib/session.ts
+++ app/src/lib/session.ts
@@ -1,10 +1,10 @@
 import { createServerFn } from '@tanstack/react-start'
-import { getRequestHeaders } from '@tanstack/start-server-core'
+import { getRequest } from '@tanstack/react-start/server'
 import { redirect } from '@tanstack/react-router'
 import { auth } from './auth'
 
 export const requireSession = async () => {
-  const session = await auth.api.getSession({ headers: getRequestHeaders() as Headers })
+  const session = await auth.api.getSession({ headers: getRequest().headers })
   if (!session) {
     throw redirect({ to: '/login' })
   }
@@ -12,7 +12,7 @@ export const requireSession = async () => {
 }
 
 export const getSession = createServerFn({ method: 'GET' }).handler(async () => {
-  const session = await auth.api.getSession({ headers: getRequestHeaders() as Headers })
+  const session = await auth.api.getSession({ headers: getRequest().headers })
   if (!session) return null
   return { userId: session.user.id, email: session.user.email }
 })
